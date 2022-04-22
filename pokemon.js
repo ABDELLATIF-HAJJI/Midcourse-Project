7958e2964795b594b2446808776f7842294a6854
Abdellatif <abdo7654ugg@gmail.com>
2022-04-22 20:51:33 +0200

    first project conclude Js

---
+++ pokemon.js
@@ -0,0 +1,101 @@
+const pokemon = [
+    {
+        thumbnail: 'img/001.png',
+        id:'Bulbasaur-id' ,
+        name: 'bulbasaur',
+        type: ['plant', 'poison']
+    },
+    {
+        thumbnail: 'img/002.png',
+        id:'Charmander-id',
+        name: 'charmander',
+        type: 'fire'
+    },
+    {
+        thumbnail: 'img/003.png',
+        id:'Caterpie-id',
+        name: 'caterpie',
+        type: 'bug'
+    },
+    {
+        thumbnail: 'img/004.png',
+        id:'-id',
+        name: 'kaluna',
+        type: 'bug'
+    },
+    {
+        thumbnail: 'img/005.png',
+        id:'Ratata-id',
+        name: 'ratata',
+        type: 'ratata'
+    },
+    {
+        thumbnail: 'img/006.png',
+        id:'Pikachu-id',
+        name: 'pikachu',
+        type: 'pikachu'
+    },
+    {
+        thumbnail: 'img/007.png',
+        id:'Vulpix-id',
+        name: 'vulpix',
+        type: 'vire'
+    },
+    {
+        thumbnail: 'img/008.png',
+        id:'Jigglypuff-id',
+        name: 'jigglypuff',
+        type: ['normal', 'fairy']
+    },
+    {
+        thumbnail: 'img/009.png',
+        id:'Ice-id',
+        name: 'ice',
+        type: 'fairy'
+    },
+    {
+        thumbnail: 'img/010.png',
+        id:'Ground-id',
+        name: 'ground',
+        type: 'fire'
+    },
+    {
+        thumbnail: 'img/011.png',
+        id:'Ghost-id',
+        name: 'ghost',
+        type: 'position'
+    },
+    {
+        thumbnail: 'img/012.png',
+        id:'Deer-id',
+        name: 'deer',
+        type: 'electrical'
+    },
+    {
+        thumbnail: 'img/013.png',
+        id:'Dark-id',
+        name: 'dark',
+        type: 'fairy'
+    },
+    {
+        thumbnail: 'img/014.png',
+        id:'Steel-id',
+        name: 'stell',
+        type: ['normal', 'electrical']
+    },
+    {
+        thumbnail: 'img/015.png',
+        id:'Cat-id',
+        name: 'cat',
+        type: 'fire'
+    },
+    {
+        thumbnail: 'img/016.png',
+        id:'Rock-id',
+        name: 'rock',
+        type: ['fairy', 'Poison']
+    },
+        
+    
+];
+console.log(pokemon)
