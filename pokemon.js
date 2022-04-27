99355b7d61b4db02a0e387c522e94b735619674a
Abdellatif <abdo7654ugg@gmail.com>
2022-04-27 23:43:06 +0200

    basic Js'DOM' added

--- pokemon.js
+++ pokemon.js
@@ -1,118 +1,146 @@
 const pokemons = [
     {
         thumbnail: 'img/001.png',
-    id:'BulbasauriId' ,
+        id:'N°01' ,
         name: 'bulbasaur',
         type: ['plant', 'poison']
     },
     {
         thumbnail: 'img/002.png',
-    id:'CharmanderiId',
+        id:'N°02',
         name: 'charmander',
-        type: 'fire'
+        type: ['fire']
     },
     {
         thumbnail: 'img/003.png',
-    id:'CaterpieiId',
+        id:'N°03',
         name: 'caterpie',
-        type: 'bug'
+        type: ['bug']
     },
     {
         thumbnail: 'img/004.png',
-    id:'iId',
+        id:'N°04',
         name: 'kaluna',
         type: ['bug', 'poison']
     },
     {
         thumbnail: 'img/005.png',
-    id:'RatataiId',
+        id:'N°05',
         name: 'ratata',
-        type: 'ratata'
+        type: ['plant']
     },
     {
         thumbnail: 'img/006.png',
-    id:'PikachuiId',
+        id:'N°06',
         name: 'pikachu',
-        type: 'pikachu'
+        type: ['normal']
     },
     {
         thumbnail: 'img/007.png',
-    id:'VulpixiId',
+        id:'N°07',
         name: 'vulpix',
         type: ['fire', 'plant']
     },
     {
         thumbnail: 'img/008.png',
-    id:'JigglypuffiId',
+        id:'N°08',
         name: 'jigglypuff',
         type: ['normal', 'fairy']
     },
     {
         thumbnail: 'img/009.png',
-    id:'IceiId',
+        id:'N°09',
         name: 'ice',
-        type: 'fairy'
+        type: ['fairy']
     },
     {
         thumbnail: 'img/010.png',
-    id:'GroundiId',
+        id:'N°10',
         name: 'ground',
-        type: 'electrical'
+        type: ['electrical']
     },
     {
         thumbnail: 'img/011.png',
-    id:'GhostiId',
+        id:'N°11',
         name: 'ghost',
-        type: 'position'
+        type: ['poison']
     },
     {
         thumbnail: 'img/012.png',
-    id:'DeeriId',
+        id:'N°12',
         name: 'deer',
-        type: 'faire'
+        type: ['fire']
     },
     {
         thumbnail: 'img/013.png',
-    id:'DarkiId',
+        id:'N°13',
         name: 'dark',
-        type: 'fairy'
+        type: ['fairy']
     },
     {
         thumbnail: 'img/014.png',
-    id:'SteeliId',
+        id:'N°14',
         name: 'stell',
         type: ['normal', 'electrical']
     },
     {
         thumbnail: 'img/015.png',
-    id:'CatiId',
+        id:'N°15',
         name: 'cat',
-        type: 'fire'
+        type: ['fire']
     },
     {
         thumbnail: 'img/016.png',
-    id:'RockiId',
+        id:'N°16',
         name: 'rock',
-        type: ['fairy', 'Poison']
+        type: ['fairy', 'poison']
     },
 ];
-//console.log(pokemon[0].type[1]);
 
-const pokemon1  = document.querySelector('#BulbassauriId');
-const pokemon2  = document.getElementBIId('CharmanderiId');
-const pokemon3  = document.querySelector('#CaterpieiId');
-const pokemon4  = document.querySelector('#KalunaiId');
-const pokemon5  = document.querySelector('#RatataiId');
-const pokemon6  = document.querySelector('#PikachuiId');
-const pokemon7  = document.querySelector('#VulpixiId');
-const pokemon8  = document.querySelector('#JigglypuffiId');
-const pokemon9  = document.querySelector('#IceiId');
-const pokemon10 = document.querySelector('#GroundiId');
-const pokemon11 = document.querySelector('#GhostiId');
-const pokemon12 = document.querySelector('#DeeriId');
-const pokemon13 = document.querySelector('#DarkiId');
-const pokemon14 = document.querySelector('#SteeliId');
-const pokemon15 = document.querySelector('#CatiId');
-const pokemon16 = document.querySelector('#RockiId');
+//logo withoutHTML//////////////////////////////////////////////////////////////
+/*const mainLogo = document.createElement('div');
+mainLogo.id = 'logo';
+document.body.append(mainLogo);
+const titleLogo = document.createElement ('img');
+titleLogo.className = 'pokemontitle';
+titleLogo.src = 'img/pokemon_logo.png';
+mainLogo.appendChild(titleLogo);
+const ballLogo = document.createElement ('img');
+ballLogo.className = 'pokeball';
+ballLogo.src = 'img/pokeball.PNG';
+mainLogo.appendChild(ballLogo);*/
+//section////////////////////////////////////////////////////////////
+const mainSection = document.querySelector('.mainSection');
 
-const firstPokemon = pokemons[0];
+for (var i=0; i<pokemons.length; i++){
+
+    const pokemoncontainer = document.createElement ('div');
+    pokemoncontainer.className = 'container';
+    mainSection.appendChild(pokemoncontainer);
+    
+    const imgContainer = document.createElement ('div');
+    imgContainer.className = 'background-img';
+    pokemoncontainer.appendChild(imgContainer);
+    
+    const image = document.createElement ('img');
+    image.className = 'content-img';
+    imgContainer.appendChild(image);
+    image.src = pokemons[i].thumbnail;
+    
+    const number = document.createElement ('span');
+    number.className = 'pokemonNumber';
+    pokemoncontainer.appendChild(number);
+    number.innerHTML = pokemons[i].id +"<br>";
+    
+    const name = document.createElement ('span');
+    name.className = 'pokemonName';
+    pokemoncontainer.appendChild(name);
+    name.innerHTML = pokemons[i].name +"<br>";
+    
+    for (var j=0; j<pokemons[i].type.length; j++){
+        const bt1 = document.createElement ('button');
+        bt1.className =  'pokemonButton';
+        pokemoncontainer.appendChild(bt1);
+        bt1.innerHTML = pokemons[i].type[j];
+    }
+}
